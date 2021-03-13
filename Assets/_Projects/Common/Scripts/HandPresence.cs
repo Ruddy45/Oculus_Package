0be3f576e57afbeb9ad0d85de975f09a7278f14f
Wesley Petit <ptitwes.45@gmail.com>
2021-03-13 16:14:26 +0100

    VR Setup and oculus hand

---
+++ Assets/_Projects/Common/Scripts/HandPresence.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.XR;
+
+// Play hand animation
+[RequireComponent(typeof(Animator))]
+public class HandPresence : MonoBehaviour
+{
+	private const string TRIGGER_ID = "Trigger";
+	private const string GRIP_ID = "Grip";
+
+	[SerializeField] InputDeviceCharacteristics _controllerCharacteristics = InputDeviceCharacteristics.Left;
+
+	private Animator _handAnimator = null;
+	private InputDevice _targetDevice = new InputDevice();
+
+	private void Start()
+	{
+		_handAnimator = GetComponent<Animator>();
+		_targetDevice = GetDevice();
+	}
+
+	private void Update() => UpdateHandAnimation();
+
+	private InputDevice GetDevice()
+	{
+		List<InputDevice> devices = new List<InputDevice>();
+
+		InputDevices.GetDevicesWithCharacteristics(_controllerCharacteristics, devices);
+		if (0 < devices.Count)
+		{
+			return devices[0];
+		}
+
+		Debug.LogError("Devices not found.");
+		return new InputDevice();
+	}
+
+	private void UpdateHandAnimation()
+	{
+		if (!_targetDevice.isValid) { return; }
+
+		TriggerAnimation();
+		GripAnimation();
+	}
+
+	private void TriggerAnimation()
+	{
+		float triggerValue = 0f;
+		if (_targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float value))
+		{
+			triggerValue = value;
+		}
+
+		_handAnimator.SetFloat(TRIGGER_ID, triggerValue);
+	}
+
+	private void GripAnimation()
+	{
+		float gripValue = 0f;
+		if (_targetDevice.TryGetFeatureValue(CommonUsages.grip, out float value))
+		{
+			gripValue = value;
+		}
+
+		_handAnimator.SetFloat(GRIP_ID, gripValue);
+	}
+}
---
+++ Assets/_Projects/Common/Scripts/SpawnControllerModel.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+// Show hand and controllers
+public class SpawnControllerModel : MonoBehaviour
+{
+	[SerializeField] private bool _showHand = false;
+	[SerializeField] private GameObject _hand = null;
+	[SerializeField] private GameObject _controllers = null;
+
+	private void Start() => SpawnHandAndController();
+
+	private void SpawnHandAndController()
+	{
+		if (_showHand)
+		{
+			GameObject spawnedHand = Instantiate(_hand, transform);
+			spawnedHand.name = _hand.name;
+		}
+		else
+		{
+			GameObject spawnedController = Instantiate(_controllers, transform);
+			spawnedController.name = _controllers.name;
+		}
+	}
+}
